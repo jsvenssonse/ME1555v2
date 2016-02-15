5ece31bf0bacc9f6c7f9a578f1e7b9c0fba9ea16
Jesper <jesper@jesper.se>
2016-02-15 15:47:05 +0100

    db

--- database/migrations/2016_02_15_135549_create_post_tag_table.php
+++ database/migrations/2016_02_15_135549_create_post_tag_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_tag', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('post_id');
+            $table->string('tag_id');
+            $table->timestamps();    
         });
     }
 
@@ -29,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('post_tag');
     }
 }
