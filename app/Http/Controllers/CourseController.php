cfcc5c853abace4e0424263c20e2f62e8eb14468
xonvanetta <xon.vanetta@gmail.com>
2016-03-02 12:48:04 +0100

    childsteps

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -18,6 +18,12 @@ class CourseController extends Controller
         $course = DB::table('courses')->where('id', $id)->first();
         $steps =  DB::table('steps')->where('parent_type','course')->where('parent_id',$id)->get();
         if(!empty($steps)){
+            foreach ($steps as $step) {
+                $childsteps = DB::table('steps')->where('parent_type','step')->where('parent_id',$step->id)->get();
+                if(!empty($childsteps)){
+                    $step->childsteps = $childsteps;
+                }
+            }
             $course->steps = $steps;
         }
         return json_encode($course);
