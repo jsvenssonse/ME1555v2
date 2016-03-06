81e3df95e669c9f6f1954888cee0a98b3d22aca3
kevintatou <kevintatou94@gmail.com>
2016-03-06 14:27:54 -0500

    fixed real data on courses and their desc

--- database/seeds/CourseTableSeeder.php
+++ database/seeds/CourseTableSeeder.php
@@ -15,13 +15,42 @@ class CourseTableSeeder extends Seeder
 
         $limit = 10;
         
-
-        for ($i = 0; $i < $limit; $i++) {
+            $collection = [
+                ['cat' => 'Programming',
+                'title' => "Visual C++", 
+                "desc" =>"Learn the basics of C++, from basic variable handling to advanced Object Oriented Concepts."
+                ],
+                ['cat' => 'Science',
+                'title' => "Atom Splitting", 
+                "desc" => "Crash course in atom splitting, DIY at home! All you need is a few home appliances, and you are set!"
+                ],
+                ['cat' => 'Planning',
+                'title' => "My Trip to Paris", 
+                "desc" => "Let me show you how I spent my time in Paris. I will introduce to you where to get the cheapeast croissants and cheapest living spaces "
+                ],
+                ['cat' => 'General IT',
+                'title' => "What is a Raspberry Pi?", 
+                "desc" => "I bought a raspberry pi and I want to know how to use it. Thought I might use it as an extra computer."
+                ],
+                ['cat' => 'Sports',
+                'title' => "Getting six pack abs", 
+                "desc" => "This is my experience and guide to how I got six pack abs in 12 weeks."
+                ],
+                ['cat' => 'Gaming',
+                'title' => "Super Mario Bros", 
+                "desc" => "My step by step guide on how to save the queen in super mario on gameboy color."
+                ],  
+            ];
+            
+            
+            $cats = $collection[0]['cat'];
+        
+        for ($i = 0; $i < 6; $i++) {
             DB::table('courses')->insert([
                 'user_id' => rand(1,10),
-                'cat' => $faker->unique()->word,
-                'title' => $faker->word,
-                'desc' => $faker->text($maxNbChars = 600),
+                'cat' => $collection[$i]['cat'],
+                'title' => $collection[$i]['title'],
+                'desc' => $collection[$i]['desc'],
             ]);
         }
     }
